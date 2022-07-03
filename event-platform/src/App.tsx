17ff05e9e3e4a95087bb0734e554bffc1c38fa92
Leonardo Scalabrin <leojscalabrin@gmail.com>
2022-07-03 17:40:30 -0300

    router e props da aula

--- event-platform/src/App.tsx
+++ event-platform/src/App.tsx
@@ -1,10 +1,16 @@
-import { Evento } from "./pages/Evento"
+import { ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./components/Router";
+import { client } from "./lib/apollo";
 
 function App() {
   return (
-    <Evento />
-  )
-
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
-export default App
+export default App;
